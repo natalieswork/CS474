432de85e867582b559d4423491de0983fb490cbf
natalieswork <natalieleecodes@gmail.com>
2023-09-14 17:32:18 -0400

    add inital sorting functions placeholders

---
+++ PessimalSortAlgs/sorting_algs.py
@@ -0,0 +1,9 @@
+def bozosort(lst: list) -> list:
+    """Docstring"""
+    # awwal write code here
+    return lst # this can be changed but return the sorted list 
+
+def sleepsort(lst: list) -> list:
+    """Docstring"""
+    # natalie write code here
+    return lst
